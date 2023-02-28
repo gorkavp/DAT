cd73be3083521a75f5a9cbd430e9c8eda440432e
gorkavp <72318822+gorkavp@users.noreply.github.com>
2023-02-28 20:17:44 +0100

    ex2 and ex2

--- ex2.hs
+++ ex2.hs
@@ -3,13 +3,19 @@ module Main where
 -- Canvia la posició p de la llista xs a v
 
 actualitzarPosicio :: Int -> a -> [a] -> [a]
-actualitzarPosicio p v xs =
-  case xs of
-    [] -> []
-    x : xs' ->
-      if p == 0
-        then v : xs'
-        else x : actualitzarPosicio (p - 1) v xs'
+-- actualitzarPosicio p v xs =
+--   case xs of
+--     [] -> []
+--     x : xs' ->
+--       if p == 0
+--         then v : xs'
+--         else x : actualitzarPosicio (p - 1) v xs'
+
+actualitzarPosicio n _ xs | n < 0 = xs
+actualitzarPosicio 0 y (x : xs) = y : xs
+actualitzarPosicio 0 _ [] = []
+actualitzarPosicio n y (x : xs) = x : actualitzarPosicio (n - 1) y xs
+actualitzarPosicio _ _ [] = []
 
 main = do
   print (actualitzarPosicio 2 5 [7, 3, 10, 8, 9])
