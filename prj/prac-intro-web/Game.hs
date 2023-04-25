228cb5c840cf9bb3c102dfbaf31e317169dffe09
gorkavp <gorkavp@gmail.com>
2023-04-25 22:04:40 +0200

    version 3

--- prj/prac-intro-web/Game.hs
+++ prj/prac-intro-web/Game.hs
@@ -33,22 +33,24 @@ gameApp = onMethod
 doGet :: Handler HandlerResponse
 doGet = do
     mbSessionState <- getSession "playState" -- retorna el valor de l'atribut "playState" de la sessió
-    let game = maybe startState (read . T.unpack) mbSessionState -- si no hi ha cap valor, retorna l'estat inicial
+    -- obtenim el valor de l'estat del joc a partir de la sessió de l'usuari
+    let game = maybe startState (read . T.unpack) mbSessionState -- si no hi ha cap valor a la sessió, retorna l'estat inicial, si hi ha un valor, retorna l'estat corresponent
     respHtml $ htmlView game -- retorna la vista amb l'estat de la sessió
 
--- funció que actualitza l'estat de la sessió amb el nou valor retornat per playText i retorna l'estat de la sessió actualitzat a la vista html
+-- funció que actualitza l'estat de la sessió obtenint la cadena de caràcters del formulari introduïda per l'usuari usant la funció lookupPostParam amb el nom del paràmetre "playText" i manteint l'estat de la sessió usant les funcions setSession i getSession amb el nom de l'atribut "playState"
 doPost :: Handler HandlerResponse
 doPost = do
-    mbvalue <- lookupPostParam "playText"
-    let egame = do -- Monad (Either T.Text)
-            value <- maybe (Left "Text obligatori") Right mbvalue
-            if T.null value then Left "Text obligatori"
-            else Right value
-    case egame of
-        Left err ->
-            respHtml $ htmlView startState
-        Right game -> do
-            setSession "playState" (playState $ playText game startState) -- actualitza l'estat de la sessió amb el nou valor retornat per playText
+    mbvalue <- lookupPostParam "playText" -- retorna el valor del paràmetre "playText" del formulari
+    case mbvalue of
+        Nothing -> -- si no hi ha cap valor
+            respRedirect "#" -- redirigeix a la mateixa pàgina
+        Just value -> do -- si hi ha un valor
+            mbSessionState <- getSession "playState"
+            let game = maybe startState (read . T.unpack) mbSessionState
+            setSession "playState" (T.pack $ show $ playText value game) -- actualitza l'estat de la sessió
+            mbSessionState2 <- getSession "playState"
+            let updatedGame = maybe startState (read . T.unpack) mbSessionState2 -- obtenim el valor de l'estat del joc a partir de la sessió de l'usuari
+            respHtml $ htmlView updatedGame -- retorna la vista amb l'estat de la sessiós
 
 -- ****************************************************************
 -- View
@@ -98,6 +100,3 @@ playString xs game = foldr playChar game xs
 -- funció que a partir d'un text i un estat inicial retorna l'estat final
 playText :: Text -> GameState -> GameState
 playText t = playString (T.unpack t)
-
-playState :: GameState -> Text
-playState (on, score) = T.pack (show (on, score))
